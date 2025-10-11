e8fbaa3430b07723f7842014179cf4508f135f68
alexandre.belettini <costaalexandre45@gmail.com>
2025-10-11 11:42:15 -0300

    Add base repo

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -7,7 +7,7 @@ import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/AlexandreXYZ/',
   plugins: [
     TanStackRouterVite({ autoCodeSplitting: true }),
     viteReact(),

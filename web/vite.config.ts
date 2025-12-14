dbbbfb31233afba1fe265aa55bd6a951f9edee2d
alexandre.belettini <costaalexandre45@gmail.com>
2025-12-14 14:10:19 -0300

    revert all

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,21 +1,24 @@
-import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
+import { devtools } from '@tanstack/devtools-vite'
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
+import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
+import { nitro } from 'nitro/vite'
 
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
-
-// https://vitejs.dev/config/
-export default defineConfig({
+const config = defineConfig({
   base: '/AlexandreXYZ/',
   plugins: [
-    TanStackRouterVite({ autoCodeSplitting: true }),
-    viteReact(),
+    devtools(),
+    nitro(),
+    // this is the plugin that enables path aliases
+    viteTsConfigPaths({
+      projects: ['./tsconfig.json'],
+    }),
     tailwindcss(),
+    tanstackStart(),
+    viteReact(),
   ],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
-    },
-  },
 })
+
+export default config

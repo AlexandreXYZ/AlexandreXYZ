d45688457b4a711d7fc3a887a118e13718f0d9e2
AlexandreXYZ <AlexandreXYZ@users.noreply.github.com>
2025-12-14 02:28:55 +0000

    deploy: 31452460dbc4b1e44949d1f6169a3872ac301ef1

--- server/chunks/_/_tanstack-start-manifest_v-WUelfJ0e.mjs
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ "routes": { "__root__": { "filePath": "/home/runner/work/AlexandreXYZ/AlexandreXYZ/web/src/routes/__root.tsx", "children": ["/"], "preloads": ["/assets/main-Yu2Ljwl1.js"], "assets": [] }, "/": { "filePath": "/home/runner/work/AlexandreXYZ/AlexandreXYZ/web/src/routes/index.tsx", "assets": [], "preloads": ["/assets/index-BQeJyHyF.js"] } }, "clientEntry": "/assets/main-Yu2Ljwl1.js" });
-export {
-  tsrStartManifest
-};

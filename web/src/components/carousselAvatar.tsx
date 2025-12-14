a1c7f4529138fa9d0fe2442ed31127d30e2ccca9
alexandre.belettini <costaalexandre45@gmail.com>
2025-12-13 23:10:43 -0300

    Recreate web add Tanstack start

--- web/src/components/carousselAvatar.tsx
+++ web/src/components/carousselAvatar.tsx
@@ -1,36 +1,35 @@
-'use client'
+"use client";
 
-import Autoplay from 'embla-carousel-autoplay'
+import Autoplay from "embla-carousel-autoplay";
 
-import { useRef } from 'react'
-import maskClassic from '../assets/masks/mask-classic.jpg'
-import mask1 from '../assets/masks/mask1.png'
-import mask6 from '../assets/masks/mask6.png'
-import mask7 from '../assets/masks/mask7.png'
-import { Carousel, CarouselContent } from '@/components/ui/carousel'
+import { useRef } from "react";
+import maskClassic from "../assets/masks/mask-classic.jpg";
+import mask1 from "../assets/masks/mask1.png";
+import mask6 from "../assets/masks/mask6.png";
+import mask7 from "../assets/masks/mask7.png";
+import { Carousel, CarouselContent } from "@/components/ui/carousel";
 
-const images = [maskClassic, mask1, mask6, mask7]
+const images = [maskClassic, mask1, mask6, mask7];
 
 export function CarouselAvatar() {
-  const plugin = useRef(
-    Autoplay({ delay: 6000, stopOnInteraction: false, playOnInit: true }),
-  )
-
-  return (
-    <Carousel
-      opts={{ loop: true }}
-      plugins={[plugin.current]}
-      className="w-full size-42"
-    >
-      <CarouselContent>
-        {images.map((image, index) => (
-          <div
-            key={index}
-            className="rounded-md  size-42 min-w-42 bg-contain bg-no-repeat bg-center border-gray-500 brightness-80"
-            style={{ backgroundImage: `url(${image})` }}
-          />
-        ))}
-      </CarouselContent>
-    </Carousel>
-  )
+	const plugin = useRef(
+		Autoplay({ delay: 6000, stopOnInteraction: false, playOnInit: true })
+	);
+	return (
+		<Carousel
+			opts={{ loop: true }}
+			plugins={[plugin.current]}
+			className="w-full size-42"
+		>
+			<CarouselContent>
+				{images.map((image, index) => (
+					<div
+						key={index}
+						className="rounded-md size-42 min-w-42 bg-contain bg-no-repeat bg-center border-gray-500 brightness-90"
+						style={{ backgroundImage: `url(${image})` }}
+					/>
+				))}
+			</CarouselContent>
+		</Carousel>
+	);
 }
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,21 +1,23 @@
-import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
+import { devtools } from '@tanstack/devtools-vite'
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
+import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
+import { nitro } from 'nitro/vite'
 
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: '/AlexandreXYZ/',
+const config = defineConfig({
   plugins: [
-    TanStackRouterVite({ autoCodeSplitting: true }),
-    viteReact(),
+    devtools(),
+    nitro(),
+    // this is the plugin that enables path aliases
+    viteTsConfigPaths({
+      projects: ['./tsconfig.json'],
+    }),
     tailwindcss(),
+    tanstackStart(),
+    viteReact(),
   ],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
-    },
-  },
 })
+
+export default config
